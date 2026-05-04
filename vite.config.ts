55e8400d2d3dc8c9ea78dcdd8825f07199a76103
cezar-4blue <cezar@4blue.com.br>
2026-05-04 17:57:20 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/credencial-app-cwb/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
